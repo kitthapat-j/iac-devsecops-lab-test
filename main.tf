1bdc6080e6b8e732beda0aa07283168b1892b88a
Kitthapat j <admin01@VM000.com>
2025-10-20 22:24:14 +0000

    update commit: Azure Resource Group and Insecure NSG

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_network_security_rule" "insecure_ssh" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "172.190.231.235/32" # <--- ช่องโหว่: เปิดสู่สาธารณะ!
+  source_address_prefix       = "10.0.0.0/24" # <--- ช่องโหว่: เปิดสู่สาธารณะ!
   destination_address_prefix  = "*"
   network_security_group_name = azurerm_network_security_group.example_nsg.name
   resource_group_name         = azurerm_resource_group.rg.name
